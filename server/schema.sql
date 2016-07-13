14f29d3916e9178748f54507c3aef33a12b241c3
lcbits <lynnc24@gmail.com>
2016-07-12 22:43:51 -0700

    complete database sprint

--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,30 @@
-CREATE DATABASE chat;
+CREATE DATABASE IF NOT EXISTS chat;
 
 USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+
+  id int NOT NULL AUTO_INCREMENT,
+  text varchar(200)  NOT NULL,
+  roomname varchar(20),
+  userid int NOT NULL,
+  PRIMARY KEY (id)
 );
 
 /* Create other tables and define schemas for them here! */
 
 
+CREATE TABLE users (
+  id int NOT NULL AUTO_INCREMENT,
+  username  varchar(40)   NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY username (username)
+);
 
+ALTER TABLE messages ADD FOREIGN KEY (userid) REFERENCES users (id);
 
-/*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
- *  to create the database and the tables.*/
+--   Execute this file from the command line by typing:
+--  *    mysql -u root < server/schema.sql
+--  *  to create the database and the tables.
 
